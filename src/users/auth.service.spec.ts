7d8198e33bc8fc03e23252715ca2b58ecf1690d7
vky5 <vky05@proton.me>
2025-01-27 14:14:10 +0530

    added user controller spec file

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -7,15 +7,26 @@ import { BadRequestException, NotFoundException } from "@nestjs/common";
 
 let service: AuthService;
 let fakeUserService: Partial<UsersService>;
-let db: Partial<User>[] = []
-
+let db: User[] = [];
 
 describe("AuthService", () => {
   beforeEach(async () => {
     fakeUserService = {
-      find: () => Promise.resolve([]),
-      create: (email: string, password: string) =>
-        Promise.resolve({ id: 1, email, password } as User),
+      find: (email: string) => {
+        const filteredUsers = db.filter((user) => user.email === email);
+        return Promise.resolve(filteredUsers);
+      },
+      create: (email: string, password: string) => {
+        const user = {
+          id: Math.floor(Math.random() * 999999),
+          email,
+          password,
+        } as User;
+
+        db.push(user);
+
+        return Promise.resolve(user);
+      },
     };
 
     const module = await Test.createTestingModule({
@@ -51,14 +62,7 @@ describe("AuthService", () => {
   });
 
   it("checks the error given if the email already exists", async () => {
-    fakeUserService.find = () =>
-      // find method will return this user and we can pass any other in the create it doesnt matter
-      Promise.resolve([
-        {
-          id: 1,
-          email: "fjaklsd@fajklsda.com",
-        } as User,
-      ]);
+    await service.signup("fjlaskdf@fjlaks.com", "fjalksdf"); // since it is already in db it will throw an error
 
     await expect(
       service.signup("fjlaskdf@fjlaks.com", "fjalksdf"),
@@ -72,30 +76,16 @@ describe("AuthService", () => {
   });
 
   it("Checks if the password is invalid", async () => {
-    fakeUserService.find = () =>
-      // find method will return this user and we can pass any other in the create it doesnt matter
-      Promise.resolve([
-        {
-          id: 1,
-          email: "fjaklsd@fajklsda.com",
-          password: "correct password",
-        } as User,
-      ]);
-
+    await service.signup("fajslkdf@fjl.com", "correct password"); // correct way to check it now
     await expect(
       service.signin("fajslkdf@fjl.com", "wrong password"),
     ).rejects.toThrow(BadRequestException);
   });
 
-  // it("checks the sigin process if the user already exists", async ()=>{
-  //   fakeUserService.find = ()=>{
-  //     Promise.resolve([
-  //       {
-  //         id: 1,
-  //         email: "fjasd@afds.com",
-  //         password: ""
-  //       }
-  //     ])
-  //   }
-  // })
+  it("checks the sigin process if the user already exists", async () => {
+    await service.signup("asdf@jkl.com", "passwd");
+
+    const user = await service.signin("asdf@jkl.com", "passwd");
+    expect(user).toBeDefined();
+  });
 });
--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,18 +1,51 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { UsersController } from './users.controller';
+import { Test, TestingModule } from "@nestjs/testing";
+import { UsersController } from "./users.controller";
+import { AuthService } from "./auth.service";
+import { UsersService } from "./users.service";
+import { User } from "./user.entity";
 
-describe('UsersController', () => {
+describe("UsersController", () => {
   let controller: UsersController;
+  let fakeAuthService: Partial<AuthService>;
+  let fakeUsersService: Partial<UsersService>;
 
   beforeEach(async () => {
+    fakeUsersService = {
+      findOne: (id: number) =>
+        Promise.resolve({
+          id,
+          email: "asdfjlk@jkl.com",
+          password: "adsfjklasdjfl",
+        } as User),
+      find: (email: string) =>
+        Promise.resolve([{ id: 1, email, password: "fasdfa" } as User]),
+      // remove: () => {},
+      // update: () => {},
+    };
+
+    fakeAuthService = {
+      // signup: () => {},
+      // signin: () => {},
+    };
+
     const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        {
+          provide: UsersService,
+          useValue: fakeUsersService,
+        },
+        {
+          provide: AuthService,
+          useValue: fakeAuthService,
+        },
+      ],
       controllers: [UsersController],
-    }).compile(); 
+    }).compile();
 
     controller = module.get<UsersController>(UsersController);
   });
 
-  it('should be defined', () => {
+  it("checks if the user controller is properly defined", () => {
     expect(controller).toBeDefined();
   });
 });
